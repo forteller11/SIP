d540e857e38b5f1b82826a8d42f4450ff42f9c87
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2021-10-02 21:30:15 -0400

    init commit

---
+++ SIP  - Super Information Tracker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SIP____Super_Information_Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
